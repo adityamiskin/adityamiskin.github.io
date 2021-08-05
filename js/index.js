7c26803ff533956a21151b7ec61578924bb377c0
adityamiskin98 <adityamiskin98@gmail.com>
2021-08-05 16:05:22 +0530

    fixing form

--- js/index.js
+++ js/index.js
@@ -20,43 +20,49 @@ const scrollFunc = () => {
 
 window.addEventListener('scroll', scrollFunc);
 
-var form = document.querySelector('.pageclip-form');
+// var form = document.querySelector('.pageclip-form');
 
-Pageclip.form(form, {
-  onSubmit: function (event) {},
-  onResponse: function (error, response) {},
-  successTemplate: '<span>Works!!!</span>'
-});
+// Pageclip.form(form, {
+//   onSubmit: function (event) {},
+//   onResponse: function (error, response) {},
+//   successTemplate: '<span>Works!!!</span>'
+// });
 
 const contactName = $('#contactName');
 const contactEmail = $('#contactEmail');
 const contactSubject = $('#contactSubject');
 const contactMessage = $('#contactMessage');
 
-var data = {
-  contactName: contactName,
-  contactEmail: contactEmail,
-  contactSubject: contactSubject,
-  contactMessage: contactMessage
-};
+const contactForm = document.querySelector('#contactForm');
 
-Pageclip.send(
-  'api_3wQ69yM1RPPLtCchAaSzaCU5P7OV9hKg',
-  'contactForm',
-  data,
-  function (error, response) {
-    setTimeout(function () {
-      $('#exampleModal').modal('show');
-      confetti({
-        zIndex: 2000,
-        particleCount: 100,
-        spread: 160
-      });
-      document.getElementById('contactForm').reset();
-    }, 1000);
+contactForm.addEventListener('submit', (e) => {
+  e.preventDefault();
 
-    setTimeout(function () {
-      $('#exampleModal').modal('hide');
-    }, 3000);
-  }
-);
+  var data = {
+    contactName: contactName.value,
+    contactEmail: contactEmail.value,
+    contactSubject: contactSubject.value,
+    contactMessage: contactMessage.value
+  };
+
+  Pageclip.send(
+    'api_3wQ69yM1RPPLtCchAaSzaCU5P7OV9hKg',
+    'contactForm',
+    data,
+    function (error, response) {
+      setTimeout(function () {
+        $('#exampleModal').modal('show');
+        confetti({
+          zIndex: 2000,
+          particleCount: 100,
+          spread: 160
+        });
+        document.getElementById('contactForm').reset();
+      }, 1000);
+
+      setTimeout(function () {
+        $('#exampleModal').modal('hide');
+      }, 3000);
+    }
+  );
+});
