6daf29818d0ca757d75c8a9e1cab233ad40d6f31
adityamiskin98 <adityamiskin98@gmail.com>
2021-08-05 15:31:23 +0530

    fixing form

--- js/index.js
+++ js/index.js
@@ -20,67 +20,67 @@ const scrollFunc = () => {
 
 window.addEventListener('scroll', scrollFunc);
 
-(function ($) {
-  'use strict';
+// (function ($) {
+//   'use strict';
 
-  var ssContactForm = function () {
-    /* local validation */
-    $('#contactForm').validate({
-      /* submit via ajax */
-      submitHandler: function (form) {
-        $.ajax({
-          type: 'POST',
-          url: 'inc/sendEmail.php',
-          data: $(form).serialize(),
-          success: function (msg) {
-            // Message was sent
-            if (msg == 'OK') {
-              setTimeout(function () {
-                $('#exampleModal').modal('show');
-                confetti({
-                  zIndex: 2000,
-                  particleCount: 100,
-                  spread: 160
-                });
-                document.getElementById('contactForm').reset();
-              }, 1000);
+//   var ssContactForm = function () {
+//     /* local validation */
+//     $('#contactForm').validate({
+//       /* submit via ajax */
+//       submitHandler: function (form) {
+//         $.ajax({
+//           type: 'POST',
+//           url: 'inc/sendEmail.php',
+//           data: $(form).serialize(),
+//           success: function (msg) {
+//             // Message was sent
+//             if (msg == 'OK') {
+//               setTimeout(function () {
+//                 $('#exampleModal').modal('show');
+//                 confetti({
+//                   zIndex: 2000,
+//                   particleCount: 100,
+//                   spread: 160
+//                 });
+//                 document.getElementById('contactForm').reset();
+//               }, 1000);
 
-              setTimeout(function () {
-                $('#exampleModal').modal('hide');
-              }, 3000);
-            }
-            // There was an error
-            else {
-              document.getElementById('face').innerText = '☹️';
+//               setTimeout(function () {
+//                 $('#exampleModal').modal('hide');
+//               }, 3000);
+//             }
+//             // There was an error
+//             else {
+//               document.getElementById('face').innerText = '☹️';
 
-              document.getElementById('exampleModalLabel').innerText =
-                'There was some issue sending your message.';
+//               document.getElementById('exampleModalLabel').innerText =
+//                 'There was some issue sending your message.';
 
-              $('#exampleModal').modal('show');
+//               $('#exampleModal').modal('show');
 
-              setTimeout(function () {
-                $('#exampleModal').modal('hide');
-              }, 2000);
-            }
-          },
-          error: function () {
-            document.getElementById('face').innerText = '☹️';
+//               setTimeout(function () {
+//                 $('#exampleModal').modal('hide');
+//               }, 2000);
+//             }
+//           },
+//           error: function () {
+//             document.getElementById('face').innerText = '☹️';
 
-            document.getElementById('exampleModalLabel').innerText =
-              'There was some issue sending your message.';
+//             document.getElementById('exampleModalLabel').innerText =
+//               'There was some issue sending your message.';
 
-            $('#exampleModal').modal('show');
+//             $('#exampleModal').modal('show');
 
-            setTimeout(function () {
-              $('#exampleModal').modal('hide');
-            }, 2000);
-          }
-        });
-      }
-    });
-  };
+//             setTimeout(function () {
+//               $('#exampleModal').modal('hide');
+//             }, 2000);
+//           }
+//         });
+//       }
+//     });
+//   };
 
-  (function ssInit() {
-    ssContactForm();
-  })();
-})(jQuery);
+//   (function ssInit() {
+//     ssContactForm();
+//   })();
+// })(jQuery);
