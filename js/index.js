b1bea46a86e6cf0bdb4d7efbb81886d2f1c73051
adityamiskin98 <adityamiskin98@gmail.com>
2022-03-24 00:12:56 +0530

    changed ui

--- js/index.js
+++ js/index.js
@@ -1,47 +1,50 @@
 $('.nav-link').on('click', function () {
-  $('.navbar-collapse').collapse('hide');
+	$('.navbar-collapse').collapse('hide');
 });
 
 $('.carousel').carousel({
-  interval: false
+	interval: false,
 });
 
 // AOS.init();
 const scrollToTopButton = document.getElementById('floating-btn');
 
 const scrollFunc = () => {
-  let y = window.scrollY;
-  if (y > 100) {
-    scrollToTopButton.className = 'show';
-  } else {
-    scrollToTopButton.className = 'hide';
-  }
+	let y = window.scrollY;
+	if (y > 100) {
+		scrollToTopButton.className = 'show';
+	} else {
+		scrollToTopButton.className = 'hide';
+	}
 };
 
 window.addEventListener('scroll', scrollFunc);
+
+const date = new Date().getFullYear();
+$('#date').text(date);
 
 // window.addEventListener('scroll', () => {
 //   var nav = document.querySelector('.navbar');
 //   nav.classList.toggle('transparent', window.scrollY > 200);
 // });
 
-var form = document.querySelector('.pageclip-form');
+// var form = document.querySelector('.pageclip-form');
 
-Pageclip.form(form, {
-  onSubmit: function (event) {},
-  onResponse: function (error, response) {
-    setTimeout(function () {
-      $('#exampleModal').modal('show');
-      confetti({
-        zIndex: 2000,
-        particleCount: 100,
-        spread: 160
-      });
-      document.getElementById('contactForm').reset();
-    }, 1000);
+// Pageclip.form(form, {
+//   onSubmit: function (event) {},
+//   onResponse: function (error, response) {
+//     setTimeout(function () {
+//       $('#exampleModal').modal('show');
+//       confetti({
+//         zIndex: 2000,
+//         particleCount: 100,
+//         spread: 160
+//       });
+//       document.getElementById('contactForm').reset();
+//     }, 1000);
 
-    setTimeout(function () {
-      $('#exampleModal').modal('hide');
-    }, 3000);
-  }
-});
+//     setTimeout(function () {
+//       $('#exampleModal').modal('hide');
+//     }, 3000);
+//   }
+// });
