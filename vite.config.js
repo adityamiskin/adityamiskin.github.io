ae050d083f452c1bcce592642e2eb95fa142bf8c
Aditya <adityamiskin98@gmail.com>
2023-10-22 20:56:36 +0530

    add: deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -7,4 +7,5 @@ import { imagetools } from 'vite-imagetools';
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react(), tailwindcss(), svgr()],
+	base: '/adityamiskin.github.io/',
 });
