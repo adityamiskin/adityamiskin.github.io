41028759e9d9d2852d03165fd7ea5e136ab8ae3f
Aditya <adityamiskin98@gmail.com>
2023-10-23 01:02:57 +0530

    added play tab

--- src/components/Play.jsx
+++ src/components/Play.jsx
@@ -1,23 +1,23 @@
+import { useEffect, useState } from "react";
+
 const Play = () => {
 
-  var gallery = []
+  const [images, setImages] = useState([])
 
+  var gallery = []
   fetch('https://res.cloudinary.com/vite-img/image/list/imgs.json')
     .then(response => response.json())
     .then(data => {
       data.resources.forEach(element => {
-        gallery.push("https://res.cloudinary.com/vite-img/q_60/" + element.public_id)
+        gallery.push("https://res.cloudinary.com/vite-img/" + element.public_id)
       });
+      setImages(gallery)
     });
-
-  console.log(gallery)
-
-  // const gallery = Object.values(import.meta.glob('../assets/images/lightroom_edits_webp/*.{webp, WEBP, png, jpg, jpeg, JPEG, PNG}', { eager: true, as: 'url' }))
 
   return (
     <div>
       <div className="w-full p-4 pb-10 mx-auto mb-10 gap-3 columns-3 columns-3xs space-y-3 w-100 ">
-        {gallery.map((image, index) => {
+        {images.map((image, index) => {
           console.log(image)
           return (
             <div key={index} className="relative flex items-start w-100">
